2942d3d37cbcdf7bd18d9655895411b6032f451d
aw4fun <aw4fun@yandex.ru>
2021-04-02 11:55:34 +0300

    ngrx-1 addedd counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngrx-test';
+
+  counter = 0;
+  updatedAt?: number;
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  increase(): void {
+    this.updatedAt = Date.now();
+    this.counter++;
+  }
+
+  decrease(): void {
+    this.updatedAt = Date.now();
+    this.counter--;
+  }
+
+  reset(): void {
+    this.updatedAt = Date.now();
+    this.counter = 0;
+  }
 }
